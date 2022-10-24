ffa89891f2f1682ce24aa732432d0fff05793bb9
David <davidongaroh@gmail.com>
2022-10-24 12:41:53 +0300

    added #create,show&update to profile

--- student-motivation-app/app/controllers/profiles_controller.rb
+++ student-motivation-app/app/controllers/profiles_controller.rb
@@ -1,3 +1,24 @@
 class ProfilesController < ApplicationController
+    def create
+        user = User.find(session[:user_id])
+        profile = user.profile.create!(profile_params)
+        render json: profile, status: :created
+    end
 
+    def show
+        profile = Profile.find(params[:id])
+        render json: profile, status: :ok
+    end
+
+    def update
+       profile = Profile.find(params[:id])
+       profile.update!(profile_params)
+       render json: profile, status: 200
+    end
+
+    private
+
+    def profile_params
+        params.permit(:bio, :profile_pic)
+    end
 end
