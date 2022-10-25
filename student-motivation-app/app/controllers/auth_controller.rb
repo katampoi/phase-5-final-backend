c616fc4762adaacb48a3b56a3912ab68c83d461b
David <davidongaroh@gmail.com>
2022-10-25 10:11:11 +0300

    add auth#login

---
+++ student-motivation-app/app/controllers/auth_controller.rb
@@ -0,0 +1,21 @@
+class AuthController < ApplicationController
+    skip_before_action :authorize, only: [:login]
+  
+    def login
+      @user = User.find_by(username: login_params[:username])
+      #User#authenticate comes from BCrypt
+      if @user && @user.authenticate(login_params[:password])
+        # encode token comes from ApplicationController
+        token = encode_token({ user_id: @user.id })
+        render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+      else
+        render json: { message: 'Invalid username or password' }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def login_params
+      params.require(:user).permit(:username, :password)
+    end
+end
