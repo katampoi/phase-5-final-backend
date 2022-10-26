49c3b1fccfd1c0069998c88935e4225b52809449
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-10-26 05:02:16 +0300

    Add CRUD operations to subscription controller

--- student-motivation-app/config/routes.rb
+++ student-motivation-app/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :wishlists
-  resources :subscriptions
-  resources :categories
-  resources :reviews
-  resources :profiles
-  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
