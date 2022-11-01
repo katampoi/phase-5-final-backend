db824668c5eb2d583f63744706c7dc5bce55d717
David <davidongaroh@gmail.com>
2022-11-01 18:07:37 +0300

    refactor render minus userserializer

--- student-motivation-app/app/controllers/users_controller.rb
+++ student-motivation-app/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # before_action :authorize_admin, only: [:create, :destroy]
   
     def show_me
-      render json: { user: UserSerializer.new(current_user) }, status: :accepted
+      render json: current_user, status: :accepted
     end
 
     def show
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       @user = User.create!(user_params)
        if @user.valid?
         @token = encode_token({ user_id: @user.id })
-        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        render json: { user: @user, jwt: @token }, status: :created
       # else
       #   render json: { error: 'failed to create user' }, status: :unprocessable_entity
       end
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
     private
   
     def user_params
-      params.require(:user).permit(:email, :username, :first_name, :last_name, :password, :password_confirmation, :user_type)
+      params.permit(:email, :username, :first_name, :last_name, :password, :password_confirmation, :user_type)
     end
 end
