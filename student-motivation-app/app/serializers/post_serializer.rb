67f324e2a3344d74eeef94b4edeffdada1067e1e
davidnene <davienesh4@gmail.com>
2022-11-02 00:22:44 +0300

    Add belongs to user association

--- student-motivation-app/app/serializers/post_serializer.rb
+++ student-motivation-app/app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :title, :category_id, :media_img, :media_vid, :content, :like, :dislike
   has_many :reviews
+  belongs_to :user
 end
