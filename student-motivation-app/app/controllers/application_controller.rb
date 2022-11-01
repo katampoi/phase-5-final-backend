dd55e220865f334db688520623cd722d75deed8d
David <davidongaroh@gmail.com>
2022-11-01 18:05:01 +0300

    store secret key in env variable

--- student-motivation-app/app/controllers/application_controller.rb
+++ student-motivation-app/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   
     def encode_token(payload)
       # should store secret in env variable
-      JWT.encode(payload, 'ni kubaad')
+      JWT.encode(payload, ENV["SECRET"])
     end
   
     def auth_header
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::API
         token = auth_header.split(' ')[1]
         # header: { 'Authorization': 'Bearer <token>' }
         begin
-          JWT.decode(token, 'ni kubaad', true, algorithm: 'HS256')
+          JWT.decode(token, ENV["SECRET"], true, algorithm: 'HS256')
         rescue JWT::DecodeError
           nil
         end
